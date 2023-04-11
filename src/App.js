ab84ae539281505dafc2fd5033ac5144d1ebd39f
Kunalhume <78203037+Kunalhume@users.noreply.github.com>
2023-04-12 00:12:28 +0530

    kunal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {ResponsiveContainer,LineChart,Line, XAxis,YAxis,CartesianGrid,Legend,Tooltip} from "recharts";
+
+const pdata =[
+  {
+    name: 'Python',
+    student: 13,
+    fees: 10
+  },
+  {
+    name: 'Javascript',
+    student: 15,
+    fees: 12
+  },
+  {
+    name: 'PHP',
+    student: 5,
+    fees: 10
+  },
+  {
+    name: 'Java',
+    student: 10,
+    fees: 5
+  },
+  {
+    name: 'C#',
+    student: 9,
+    fees: 4
+  },
+  {
+    name: 'C++',
+    student: 10,
+    fees: 8
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 className="chart-header">Line Chart</h1>
+    <ResponsiveContainer width="100%" aspect={3}>
+      <LineChart data={pdata} width={500} height={300} margin={{
+        top :5, right:100, left:20 , bottom:5
+      }}>
+        <CartesianGrid strokeDasharray="4 3" />
+        <XAxis dataKey="name" interval={'preserveStartEnd'} tickFormatter={(value)=>value +" Prog."}/>
+        <YAxis />
+        <Tooltip contentStyle={{ backgroundColor: 'lightyellow' }}/>
+        <Legend />
+        <Line type="monotone" dataKey="student" stroke='red' activeDot={{r :6}}/>
+        <Line type="monotone" dataKey="fees" stroke='green' activeDot={{r :6}}/>
+      </LineChart>
+    </ResponsiveContainer>
+    </>
   );
 }
 
